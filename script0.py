01f827dea532a0eccc2c777558a7a353dca0c2da
dsug <d.zolnow@gmail.com>
2017-10-02 15:35:45 +0200

    HelloWorld!

---
+++ script0.py
@@ -0,0 +1,13 @@
+#########################
+#                       #
+#   Strings Chapter 3   #
+#                       #
+#########################
+
+# Sequence Operations
+
+S = 'Spam'          # Make a 4-character string, and assign it to a name
+print(len(S))              # Length
+print(S[0])         # The first item in S, indexing by zero-based-position
+print(S[1])         # The second item from the left
+
